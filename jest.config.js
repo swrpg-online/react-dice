44845319d63ee29231041734fa6c7236b79408bc
aybanda <ajaybandaru17@gmail.com>
2025-04-19 22:44:47 +0530

    fix: resolve asset loading from @swrpg-online/art package

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  moduleNameMapper: {
-    '\\.svg$': '<rootDir>/__mocks__/fileMock.js',
-    '@swrpg-online/art/(.*)': '<rootDir>/__mocks__/fileMock.js'
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  }
 }; 
--- rollup.config.js
+++ rollup.config.js
@@ -2,47 +2,30 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
-import { readFileSync } from 'fs';
-
-const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.main,
+      file: 'dist/index.js',
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: pkg.module,
+      file: 'dist/index.esm.js',
       format: 'esm',
       sourcemap: true,
     },
   ],
+  external: ['react', 'react-dom', '@swrpg-online/art'],
   plugins: [
-    resolve({
-      // This allows us to resolve packages from node_modules
-      moduleDirectories: ['node_modules'],
-      // Allow resolving SVG files from node_modules
-      extensions: ['.js', '.ts', '.tsx', '.svg', '.png'],
-      // Make sure we can resolve the @swrpg-online/art package
-      resolveOnly: [
-        /^(?!@swrpg-online\/art)/  // Process all imports EXCEPT @swrpg-online/art
-      ]
-    }),
     url({
       include: ['**/*.svg', '**/*.png'],
-      limit: 0, // Always generate files
-      publicPath: '',
-      destDir: 'dist/assets',
-      fileName: '[name][extname]',
+      limit: 0,
+      emitFiles: true,
     }),
+    resolve(),
     commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      exclude: ['**/*.test.ts', '**/*.test.tsx'],
-    }),
+    typescript(),
   ],
-  external: ['react', 'react-dom'], // Remove @swrpg-online/art from externals
 }; 
